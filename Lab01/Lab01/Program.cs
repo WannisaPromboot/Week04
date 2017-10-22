c41fd57b6230f89f6decb6020252662f23574d3b
Wannamiu <58030218@kmitl.ac.th>
2017-10-22 19:24:27 +0700

    commited by wannisa

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,25 +7,11 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Data type : ulong ");
+            Console.WriteLine("Size :" + sizeof(ulong));
+            Console.WriteLine("Minimum Value :" + ulong.MinValue);
+            Console.WriteLine("Maximum Value :" + ulong.MaxValue);
 
-
-            Console.Write("Your FistName:");
-            string FName = Console.ReadLine();
-
-            Console.Write("Your LastName:");
-            string LName = Console.ReadLine();
-
-            Console.Write("Your ID:");
-            int ID = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Your GPA:");
-            float GPA = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("First Name: " + FName);
-            Console.WriteLine("Last Name: " + LName);
-            Console.WriteLine("ID: {0,0:G} ", ID);
-            Console.WriteLine("GPA: {0:0.00} ", GPA);
-             
 
         }
     }
