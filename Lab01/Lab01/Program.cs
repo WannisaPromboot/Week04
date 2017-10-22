4322510c1c7a622ca25a398370f8cb087364d48f
Wannamiu <58030218@kmitl.ac.th>
2017-10-22 21:22:56 +0700

    commited by wannisa

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,20 +7,40 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            bool A, B, Y;
-            Console.WriteLine("      Y = A AND B");
-            Console.WriteLine("-----------------------");
-            Console.WriteLine("   A      B\t|  Y");
-            Console.WriteLine("-----------------------");
-            A = false; B = false; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = false; B = true; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = false; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = true; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            Console.WriteLine("-----------------------");
+            int a = 10, b = 20, x = 5, y = 2;
+
+            Console.WriteLine("a = 10, b = 20, x = 5, y = 2 ");
+            int answer1 = a + b;
+            Console.WriteLine("a + b={0}  ", answer1);
+
+            int answer2 = x - b;
+            Console.WriteLine("x - b={0}  ", answer2);
+
+            int answer3 = x * b;
+            Console.WriteLine("x * b={0}  ", answer3);
+
+            int answer4 = y / a;
+            Console.WriteLine("y / a={0}  ", answer4);
+
+            int answer5 = b % y;
+            Console.WriteLine("b % y={0}  ", answer5);
+
+            int answer6 = y + 10 % x;
+            Console.WriteLine("y + 10 % x={0}  ", answer6);
+           
+
+            int answer7 = a / 3 * 5;
+            Console.WriteLine("a / 3 * 5={0}  ", answer7);
+
+            int answer8 = 9 / 2 * a;
+            Console.WriteLine("9 / 2 * a={0}  ", answer8);
+
+            int answer9 = y % 8;
+            Console.WriteLine("y % 8={0}  ", answer9);
+
+            int answer10 = 100 * x + y % 2 - a;
+            Console.WriteLine("100 * x + y % 2 - a={0}  ", answer10);
+
 
 
 
