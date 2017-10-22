7c070d04add545ee853e2ca2f7d4fdc8f43e2901
Wannamiu <58030218@kmitl.ac.th>
2017-10-22 18:57:37 +0700

    commited by wannisa exersise

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,11 +7,25 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            string str;
-            Console.Write("Please enter value 1 : ");
-            string val1 = Console.ReadLine();
-            Console.WriteLine(val1);
 
+
+            Console.Write("Your FistName:");
+            string FName = Console.ReadLine();
+
+            Console.Write("Your LastName:");
+            string LName = Console.ReadLine();
+
+            Console.Write("Your ID:");
+            int ID = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Your GPA:");
+            float GPA = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("First Name: " + FName);
+            Console.WriteLine("Last Name: " + LName);
+            Console.WriteLine("ID: {0,0:G} ", ID);
+            Console.WriteLine("GPA: {0:0.00} ", GPA);
+             
 
         }
     }
